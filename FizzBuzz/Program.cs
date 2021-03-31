52e5f0e83444b9e99402f6db0884dc229ca927b6
Jeffrey Breiner <jeffreybreiner99@gmail.com>
2021-03-31 12:12:43 -0400

    initial commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      for (int i = 0; i < 101; i++)
+      {
+        string fizzbuzz = "";
+        if (i % 3 == 0 && i != 0)
+        {
+          fizzbuzz += "Fizz";
+        }
+        if (i % 5 == 0 && i != 0)
+        {
+          fizzbuzz += "Buzz";
+        }
+        Console.WriteLine($"{i} {fizzbuzz}");
+      }
+    }
+  }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using sc = System.Console;
+
+namespace Revature.Week1.HelloWorld
+{
+  //namespaces can only have class, struct, interface, and enum in terms of scope
+  class Program
+  {
+    private static void Main()
+    {
+      sc.WriteLine($"You suck {sc.ReadLine()}!");
+    }
+    private void UiPath()
+    {
+
+    }
+  }
+}
